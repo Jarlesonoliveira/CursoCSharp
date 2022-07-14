9632cec1df03beb2a7c9e1cfacbc2cdea0017120
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-07-14 13:43:11 -0300

    arquivo criado e sentença if feita

--- EstruturasDeControle/EstruturaIf.cs
+++ EstruturasDeControle/EstruturaIf.cs
@@ -5,7 +5,28 @@ using System.Text;
 namespace CursoCSharp.EstruturasDeControle {
     class EstruturaIf {
         public static void Executar() {
+            /*No "if" sempre a expressão irar retornar verdadeiro ou falso,
+            é a expresão if for verdadeira o que está em {sentença} a sentença sera realiza
+             ou caso seja falsa não sera realizasa a sentença*/
+            bool bomComportamento = false;
+            string entrada;
 
+            Console.Write("Digite a nota do aluno: ");
+            entrada = Console.ReadLine();
+            Double.TryParse(entrada, out double nota);
+
+            Console.WriteLine("Possui bom comportamento (S/N): ");
+            entrada = Console.ReadLine();
+
+            //if(entrada == "S" || entrada == "s")
+            //   bomComportamento = true;
+
+            bomComportamento = entrada == "S" || entrada == "s";
+            bomComportamento = entrada.ToLower() == "s";
+
+            if(nota >= 9.0 && bomComportamento) {
+                Console.WriteLine("Quadro de honra!");
+            }
         }
     }
 }
