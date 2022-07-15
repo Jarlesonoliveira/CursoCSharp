865f706e0c83d2a5cd997b3407361035aded0228
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-07-15 14:05:59 -0300

    adicionando arquivo e montando o laço for

--- EstruturasDeControle/EstruturaFor.cs
+++ EstruturasDeControle/EstruturaFor.cs
@@ -5,7 +5,10 @@ using System.Text;
 namespace CursoCSharp.EstruturasDeControle {
     class EstruturaFor {
         public static void Executar() {
-
+            //Laço for poder ser feito com uma quantidade determinada
+            for (int i = 1; i < 10; i++) {
+                Console.WriteLine($"O valor de i é {i}.");
+            }
         }
     }
 }
