b20ee3152d8709cf9a772026c06cb746a04df095
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-07-15 17:37:36 -0300

    arquivo continue é adicionando exemplo

--- EstruturasDeControle/EstruturaContinue.cs
+++ EstruturasDeControle/EstruturaContinue.cs
@@ -5,7 +5,22 @@ using System.Text;
 namespace CursoCSharp.EstruturasDeControle {
     class EstruturaContinue {
         public static void Executar() {
+            int intervalo = 50;
+            Console.WriteLine("Numeros pares entre 1 {0}!", intervalo);
 
+            for (int i = 1; i <= intervalo; i++) {
+                if (i % 2 == 1) {
+                    continue;
+                }
+
+                Console.Write(i + " ");
+            }
+
+            for (int i = 2; i <= intervalo; i += 2) {
+                Console.Write(i + " ");
+            }
+
+            
         }
     }
 }
