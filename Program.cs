bb4b8b4c6471c35d8c412c0e6f6273d73bc421c8
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-07-19 16:38:41 -0300

    adicionando arquivo GetSet com exemplos e alteração no arquivo ParametrosNomeados

--- Program.cs
+++ Program.cs
@@ -48,6 +48,7 @@ namespace CursoCSharp {
                 {"Atributos Estaticos - Classes e Métodos ", AtributosEstaticos.Executar},
                 {"Desafio Atributo  - Classes e Métodos ", DesafioAtributo.Executar},
                 {"Params - Classes e Métodos ", Params.Executar},
+                {"Get Set - Classes e Métodos ", GetSet.Executar},
             });
 
             central.SelecionarEExecutar();
