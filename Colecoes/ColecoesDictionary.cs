8657aff5d5491c634c641e9f6124a8af3dbaf534
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-07-21 15:08:04 -0300

    adicionando 1 arquivo Dictionary

---
+++ Colecoes/ColecoesDictionary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Colecoes {
+    class ColecoesDictionary {
+        public static void Execute() {
+
+        }
+    }
+}
--- Colecoes/ColecoesStack.cs
+++ Colecoes/ColecoesStack.cs
@@ -1,11 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Text;
 
 namespace CursoCSharp.Colecoes {
-    class ColecoesStack {
+    class ColecoesStack {//Stack e o contrario de fila e motodo de pilha
         public static void Executar() {
+            var pilha = new Stack();//Não ultiliza o Generic se for instanciar com new
+            //Em pilha o Push é para adicionar a pilha
+            pilha.Push(3);
+            pilha.Push("a");
+            pilha.Push(true);
+            pilha.Push(3.14f);
 
+            foreach(var item in pilha) {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine($"\nPop: {pilha.Pop()}");
+
+            foreach (var item in pilha) {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine($"\nPeek: {pilha.Peek()}");
+            Console.WriteLine(pilha.Count);
         }
     }
 }
