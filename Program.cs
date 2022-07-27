4a631d2edcc33a3a49418b6407e15626be6427aa
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-07-27 09:19:21 -0300

    Adicionando uma nova pasta api e o arquivo comimprementação de como criar e apendar novos textos

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using CursoCSharp.Colecoes;
 using CursoCSharp.OO;
 using CursoCSharp.MetodosEFuncoes;
 using CursoCSharp.Excecoes;
+using CursoCSharp.Api;
 
 namespace CursoCSharp {
     class Program {
@@ -91,6 +92,9 @@ namespace CursoCSharp {
                 //Exceções
                 {"Primeira Exceção - Exceções", PrimeiraExcecao.Executar},
                 {"Execeções Personalizadas - Exceções", ExcecoesPersonalizadas.Executar},
+
+                //Api
+                {"Primeiro Arquivo - Api", PrimeiroArquivo.Executar},
 
             });
 
